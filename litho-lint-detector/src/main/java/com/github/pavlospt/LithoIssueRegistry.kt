bd6911b7a47ffdb76ee2365aabcf9c666071140b
pavlospt <p.tournaris@gmail.com>
2017-06-01 15:37:08 +0300

    Convert LithoIssueRegistry to Kotlin

---
+++ litho-lint-detector/src/main/java/com/github/pavlospt/LithoIssueRegistry.kt
@@ -0,0 +1,15 @@
+package com.github.pavlospt
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.Issue
+import com.github.pavlospt.misc.IssuesInfo
+import java.util.Arrays
+
+class LithoIssueRegistry : IssueRegistry() {
+
+  override fun getIssues(): List<Issue> {
+    return listOf(IssuesInfo.LAYOUT_SPEC_NAME_ISSUE,
+        IssuesInfo.ANNOTATED_METHOD_VISIBILITY_ISSUE, IssuesInfo.COMPONENT_CONTEXT_NAME_ISSUE_ISSUE,
+        IssuesInfo.OPTIONAL_PROP_BEFORE_REQUIRED_ISSUE)
+  }
+}
